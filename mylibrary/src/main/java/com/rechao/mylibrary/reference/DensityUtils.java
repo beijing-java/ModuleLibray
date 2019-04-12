4729e4592b3ddf263aa3527be5281bfeb1b70d58
BeiJing爪哇 <727906773@qq.com>
2019-04-12 11:18:25 +0800

    # 更改mylibrary

---
+++ mylibrary/src/main/java/com/rechao/mylibrary/reference/DensityUtils.java
@@ -0,0 +1,85 @@
+package com.rechao.mylibrary.reference;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.res.Resources;
+import android.util.DisplayMetrics;
+import android.util.TypedValue;
+
+/**
+ * dp、px、sp相互转换方法工具类
+ *
+ * @author zjy
+ * @date 2017/3/28
+ */
+
+public class DensityUtils {
+
+    /**
+     * 屏幕密度,系统源码注释不推荐使用
+     */
+    public static final float DENSITY = Resources.getSystem()
+            .getDisplayMetrics().density;
+
+    /**
+     * 简化版的dip2px，仿kotlin的Anko写法
+     * 多次使用的分辨率采用静态初始化，加快点运行速度
+     *
+     * @return dp转化的px值
+     */
+    public static int dp(float dp) {
+        return (int) (dp * DENSITY + 0.5f);
+    }
+
+    /**
+     * 根据手机的分辨率从 dp 的单位 转成为 px(像素)
+     */
+    @Deprecated
+    public static int dip2px(Context context, float dpValue) {
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (dpValue * scale + 0.5f);
+    }
+
+    /**
+     * 根据手机的分辨率从 px(像素) 的单位 转成为 dp
+     */
+    public static int px2dip(float pxValue) {
+        return (int) (pxValue / DENSITY + 0.5f);
+    }
+
+    /**
+     * 将px值转换为sp值，保证文字大小不变
+     */
+    public static int px2sp(Context context, float pxValue) {
+        final float fontScale = context.getResources().getDisplayMetrics().scaledDensity;
+        return (int) (pxValue / fontScale + 0.5f);
+    }
+
+    /**
+     * 将sp值转换为px值，保证文字大小不变
+     */
+    public static int sp2px(Context context, float spValue) {
+        final float fontScale = context.getResources().getDisplayMetrics().scaledDensity;
+        return (int) (spValue * fontScale + 0.5f);
+    }
+
+    /**
+     * 将sp值转换为px值，保证文字大小不变
+     */
+    public static int sp2px(Context context, int sp) {
+        return (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_PX, sp, context.getResources().getDisplayMetrics());
+    }
+    // 屏幕宽度（像素）
+    public static  int getWindowWidth(Activity context){
+        DisplayMetrics metric = new DisplayMetrics();
+        context.getWindowManager().getDefaultDisplay().getMetrics(metric);
+        return metric.widthPixels;
+    }
+
+    // 屏幕高度（像素）
+    public static int getWindowHeight(Activity activity){
+        DisplayMetrics metric = new DisplayMetrics();
+        activity.getWindowManager().getDefaultDisplay().getMetrics(metric);
+        return metric.heightPixels;
+    }
+}
